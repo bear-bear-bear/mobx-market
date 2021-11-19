f10859ed8fc25fc8a9ab1bfd5ca16dc794e9cca0
bearbearbear <ghlwkdthfl@gmail.com>
2021-11-20 02:49:10 +0900

    feat: App 에 Counter 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-// src/App.jsx
 import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <Counter />
+        </div>
+    );
 }
 
 export default App;
